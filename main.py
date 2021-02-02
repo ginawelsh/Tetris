c6c7aa8148d8e0bddfb104bb3ad9d337e41bb56a
ginawelsh <gina.welsh@anu.edu.au>
2021-02-02 14:31:06 +1100

    fixed window

--- main.py
+++ main.py
@@ -17,51 +17,6 @@ shapes: S, Z, I, O, J, L, T
 represented in order by 0 - 6
 """
 
-# Initialise the game engine
-pygame.init()
-
- 
-# Define the colors we will use in RGB format
-BLACK = (  0,   0,   0)
-WHITE = (255, 255, 255)
-BLUE =  (  0,   0, 255)
-GREEN = (  0, 255,   0)
-RED =   (255,   0,   0)
-
-# Set the height and width of the screen
-size = [400, 300]
-screen = pygame.display.set_mode(size)
-
-pygame.display.set_caption("Example code for the draw module")
-
-#Loop until the user clicks the close button.
-done = False
-clock = pygame.time.Clock()
-
-while not done:
- 
-    # This limits the while loop to a max of 10 times per second.
-    # Leave this out and we will use all CPU we can.
-    clock.tick(10)
-     
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done=True # Flag that we are done so we exit this loop
- 
-    # All drawing code happens after the for loop and but
-    # inside the main while done==False loop.
-     
-    # Clear the screen and set the screen background
-    screen.fill(WHITE)
-
-    # Go ahead and update the screen with what we've drawn.
-    # This MUST happen after all the other drawing commands.
-    pygame.display.flip()
- 
-# Be IDLE friendly
-pygame.quit()
-
-
 # GLOBAL VARS
 s_width = 800
 s_height = 700
@@ -178,8 +133,57 @@ T = [['.....',
       '.....']]
 
 shapes = [S, Z, I, O, J, L, T]
-shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
+# shape_colors = [white, black, red, blue, green, yellow, orange]
 # index 0 - 6 represent shape
+
+
+# initialise game machine
+
+pygame.init()
+
+# Set the height and width of the screen
+size = [400, 300]
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Tetris Game (CodaBunga version)")
+
+# Define the colors we will use in RGB format (need to declare after game is init?)
+
+WHITE = (255, 255, 255)
+BLACK  = (0, 0, 0)
+RED = (255, 0, 0, 125)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+ORANGE = (255, 128, 0)
+
+
+
+done = False
+clock = pygame.time.Clock()
+
+while not done:
+ 
+    # This limits the while loop to a max of 10 times per second.
+    # Leave this out and we will use all CPU we can.
+    clock.tick(10)
+     
+    for event in pygame.event.get(): # User did something
+        if event.type == pygame.QUIT: # If user clicked close
+            done=True # Flag that we are done so we exit this loop
+ 
+    # All drawing code happens after the for loop and but
+    # inside the main while done==False loop.
+     
+    # Clear the screen and set the screen background
+    screen.fill(WHITE)
+
+
+    # Go ahead and update the screen with what we've drawn.
+    # This MUST happen after all the other drawing commands.
+    pygame.display.flip()
+ 
+# Be IDLE friendly
+pygame.quit()
 
 
 class Piece(object):
